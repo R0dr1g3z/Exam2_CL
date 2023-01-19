b1e464eb5869610834155127ce737808e3b8db9c
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-19 11:41:26 +0100

    add comments

--- src/main/java/com/example/Main01.java
+++ src/main/java/com/example/Main01.java
@@ -1,37 +1,37 @@
 package com.example;
 
 public class Main01 {
-    private static final String QUERY1 = "CREATE Table users(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(60),
-        email VARCHAR(60) UNIQUE,
-        password VARCHAR(60)
-    );";
-    private static final String QUERY2 = "CREATE Table messages(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        user_id INT,
-        message TEXT,
-        FOREIGN KEY (user_id) REFERENCES users (id)
-    );";
-    private static final String QUERY3 = "CREATE Table items(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(40),
-        description TEXT,
-        price DECIMAL(7,2)
-    );";
-    private static final String QUERY4 = "CREATE Table orders(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        user_id INT,
-        description TEXT,
-        created DATE,
-        FOREIGN KEY (user_id) REFERENCES users (id)
-    );";
-    private static final String QUERY5 = "CREATE Table items_orders(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        item_id INT,
-        order_id INT,
-        FOREIGN KEY (item_id) REFERENCES items (id),
-        FOREIGN KEY (order_id) REFERENCES orders (id)
-    );";
+    // private static final String QUERY1 = "CREATE Table users(
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     name VARCHAR(60),
+    //     email VARCHAR(60) UNIQUE,
+    //     password VARCHAR(60)
+    // );";
+    // private static final String QUERY2 = "CREATE Table messages(
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     user_id INT,
+    //     message TEXT,
+    //     FOREIGN KEY (user_id) REFERENCES users (id)
+    // );";
+    // private static final String QUERY3 = "CREATE Table items(
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     name VARCHAR(40),
+    //     description TEXT,
+    //     price DECIMAL(7,2)
+    // );";
+    // private static final String QUERY4 = "CREATE Table orders(
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     user_id INT,
+    //     description TEXT,
+    //     created DATE,
+    //     FOREIGN KEY (user_id) REFERENCES users (id)
+    // );";
+    // private static final String QUERY5 = "CREATE Table items_orders(
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     item_id INT,
+    //     order_id INT,
+    //     FOREIGN KEY (item_id) REFERENCES items (id),
+    //     FOREIGN KEY (order_id) REFERENCES orders (id)
+    // );";
     
 }
